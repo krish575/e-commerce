b3706e9c1d7eb6aa9085f912fda409c36551c7b6
krish575 <krishpat2016@gmail.com>
2023-09-18 14:03:59 +0530

    latest commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import Component_navbar from './components/Component_navbar';
+import Component_heroBanner from './components/Component_heroBanner';
+
+function App() {
+  return (
+    <>
+      <Component_navbar/>
+      <Component_heroBanner/>
+    </>
+  )
+}
+
+export default App
